18ccbc1c3c09e1b20e6ac5b7a62749c73d00e28d
alexmancheno <alex.mancheno@gmail.com>
2017-06-03 12:48:16 -0400

    update

---
+++ MC-Forum/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MC_Forum.Models;
+
+namespace MC_Forum.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                return View(db.UserAccount.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDbContext db = new OurDbContext())
+                {
+                    db.UserAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + " successfully registered.";
+            }
+            return View();
+        }
+
+        // Login 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                var usr = db.UserAccount.Single(u => u.Username == user.Username && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password is wrong.");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+    }
+}
