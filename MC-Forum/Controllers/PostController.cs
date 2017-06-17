40f6fa89c0f6ef4946f43307deb018ea3e939d49
alexmancheno <alex.mancheno@gmail.com>
2017-06-16 21:08:41 -0400

    fixed some css issues

---
+++ MC-Forum/Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MC_Forum.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submit_Post()
+        {
+            return View();
+        }
+    }
+}
